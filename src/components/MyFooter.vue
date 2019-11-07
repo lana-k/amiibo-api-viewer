e0b8386c9340831f67727c7e79d3130a7ec48b40
lana-k <skochetova89@gmail.com>
2019-11-07 18:04:22 +0100

    create MyFooter component

---
+++ src/components/MyFooter.vue
@@ -0,0 +1,51 @@
+<template>
+  <footer >
+    <ul>
+      <li v-for="(category, index) in categories" :key="index">
+        <router-link :to="`/categories/${category}`">{{ category }}</router-link>
+      </li>
+    </ul>
+  </footer>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { API } from '../utils'
+
+export default Vue.extend({
+  name: 'MyFooter',
+  data () {
+    return {
+      categories: []
+    }
+  },
+  created () {
+    API.getcategories()
+      .then(
+        (data) => {
+          this.categories = data
+        }
+      )
+      .catch(
+        function (err) {
+          console.log('Fetch error', err)
+        }
+      )
+  }
+})
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+footer {
+  background-color: #636363;
+  color: #fff;
+  grid-row-start: 2;
+  grid-row-end: 3;
+}
+a {
+  color: #fff;
+}
+
+</style>
