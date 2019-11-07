42f0f55d5f0e3c652b94b001692e3a84126653b7
lana-k <skochetova89@gmail.com>
2019-11-07 18:05:35 +0100

    Show categories on Categories page

--- src/views/Categories.vue
+++ src/views/Categories.vue
@@ -1,11 +1,37 @@
 <template>
   <div class="page">
-    <h1>This is a categories page</h1>
+    <h1>Categories</h1>
+    <ul>
+      <li v-for="(category, index) in categories" :key="index">
+        <router-link :to="`/categories/${category}`">{{ category }}</router-link>
+      </li>
+    </ul>
   </div>
 </template>
 
-<script>
-export default {
-  name: 'Categories'
-}
+<script lang="ts">
+import Vue from 'vue'
+import { API } from '../utils'
+
+export default Vue.extend({
+  name: 'Categories',
+  data () {
+    return {
+      categories: []
+    }
+  },
+  created () {
+    API.getcategories()
+      .then(
+        (data) => {
+          this.categories = data
+        }
+      )
+      .catch(
+        function (err) {
+          console.log('Fetch error', err)
+        }
+      )
+  }
+})
 </script>
