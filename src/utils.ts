5e9bf6ea5bfae8f1d5f2e43b7940b4e33e592786
lana-k <skochetova89@gmail.com>
2019-11-07 15:50:36 +0100

    show item on Random page

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,7 @@
 class API {
   static baseURL:string = 'https://api.publicapis.org/'
 
-  static getEntries (params: object) {
+  static getEntries (params?: object) {
     var url = new URL(`${this.baseURL}entries`)
     url.search = new URLSearchParams(<URLSearchParams>params).toString()
     return fetch(url.toString())
@@ -16,7 +16,7 @@ class API {
       })
   }
 
-  static getRandom (params: object) {
+  static getRandom (params?: object) {
     var url = new URL(`${this.baseURL}random`)
     url.search = new URLSearchParams(<URLSearchParams>params).toString()
     return fetch(url.toString())
--- src/views/Random.vue
+++ src/views/Random.vue
@@ -1,11 +1,52 @@
 <template>
   <div class="page">
-    <h1>This is a random page</h1>
+    <h1>{{ record.API }}</h1>
+    <div v-if="record.Auth">Auth: {{ record.Auth }}</div>
+    <p>{{ record.Description }}</p>
+    <div>Category: {{ record.Category }}</div>
+    <a :href="record.Link">Link</a>
+    <button @click='fetchData'>Get another random item</button>
   </div>
 </template>
 
-<script>
-export default {
-  name: 'Random'
-}
+<script lang="ts">
+import Vue from 'vue'
+import { API } from '../utils'
+
+export default Vue.extend({
+  name: 'Random',
+  data () {
+    return {
+      record: {
+        API: '',
+        Auth: '',
+        Description: '',
+        HTTPS: true,
+        Cors: '',
+        Link: '',
+        Category: ''
+      }
+    }
+  },
+  created () {
+    this.fetchData()
+  },
+  methods: {
+    /**
+    * Gets random item.
+    *
+    * @remarks
+    * Gets random item and save it in `record`.
+    */
+    fetchData () {
+      API.getRandom()
+        .then(data => {
+          this.record = data.entries[0]
+        })
+        .catch(function (err) {
+          console.log('Fetch error', err)
+        })
+    }
+  }
+})
 </script>
