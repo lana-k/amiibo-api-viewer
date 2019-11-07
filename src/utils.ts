7f109fcec8526d0aacfb0580afa07cc2f105621c
lana-k <skochetova89@gmail.com>
2019-11-07 15:33:01 +0100

    fix type of params in API class

--- src/utils.ts
+++ src/utils.ts
@@ -1,9 +1,9 @@
 class API {
   static baseURL:string = 'https://api.publicapis.org/'
 
-  static getEntries (params:URLSearchParams) {
+  static getEntries (params: object) {
     var url = new URL(`${this.baseURL}entries`)
-    url.search = new URLSearchParams(params).toString()
+    url.search = new URLSearchParams(<URLSearchParams>params).toString()
     return fetch(url.toString())
       .then(function (response) {
         if (response.status !== 200) {
@@ -16,9 +16,9 @@ class API {
       })
   }
 
-  static getRandom (params:URLSearchParams) {
+  static getRandom (params: object) {
     var url = new URL(`${this.baseURL}random`)
-    url.search = new URLSearchParams(params).toString()
+    url.search = new URLSearchParams(<URLSearchParams>params).toString()
     return fetch(url.toString())
       .then(function (response) {
         if (response.status !== 200) {
