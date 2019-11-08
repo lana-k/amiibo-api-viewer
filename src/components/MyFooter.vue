06e9ac1e445058e30b7fe9745a527c07ee734879
lana-k <skochetova89@gmail.com>
2019-11-08 22:42:13 +0100

    styles for footer

--- src/components/MyFooter.vue
+++ src/components/MyFooter.vue
@@ -1,10 +1,21 @@
 <template>
   <footer>
-    <ul>
-      <li v-for="(category, index) in categories" :key="index">
-        <router-link :to="`/categories/${category.name}`">{{ category.name }}</router-link>
-      </li>
-    </ul>
+    <div class="footer-content">
+      <div class="footer-column">
+        <h3><router-link to="/">HOME</router-link></h3>
+      </div>
+      <div class="footer-column">
+        <h3><router-link to="/categories">CATEGORIES</router-link></h3>
+          <ul>
+            <li v-for="(category, index) in categories" :key="index">
+              <router-link :to="`/categories/${category.name}`">{{ category.name }}</router-link>
+            </li>
+          </ul>
+      </div>
+      <div class="footer-column">
+        <h3><router-link to="/random">RANDOM</router-link></h3>
+      </div>
+    </div>
   </footer>
 </template>
 
@@ -41,11 +52,29 @@ export default Vue.extend({
 footer {
   background-color: #636363;
   color: #fff;
+  display: grid;
+  grid-template-columns: auto minmax(320px, 640px) auto ;
   grid-row-start: 2;
   grid-row-end: 3;
 }
+.footer-content {
+  grid-column-start: 2;
+  display: grid;
+  grid-template-columns: auto auto auto;
+}
+.footer-column {
+  padding: 8px;
+  box-sizing: border-box;
+}
 a {
   color: #fff;
+}
+ul {
+  padding: 0px;
+  list-style: none;
+}
+li {
+  margin: 8px;
 }
 
 </style>
